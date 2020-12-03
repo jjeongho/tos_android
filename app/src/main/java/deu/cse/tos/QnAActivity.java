cf82fd4145a89b0d0ead21dda6bb607492a39cc9
Chaebin Park <coqls0219@naver.com>
2020-12-04 00:10:20 +0900

    중간점검

--- app/src/main/java/deu/cse/tos/QnAActivity.java
+++ app/src/main/java/deu/cse/tos/QnAActivity.java
@@ -2,6 +2,7 @@ package deu.cse.tos;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,15 +11,23 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class QnAActivity extends AppCompatActivity {
     private Context mContext = QnAActivity.this;
     private static final int ACTIVITY_NUM = 1;
     private static final String TAG = "QnAActivity";
+    private RecyclerView listview;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +46,42 @@ public class QnAActivity extends AppCompatActivity {
         setContentView(R.layout.activity_qna);
         setupBottomNavigationView();
 
+        init();
     }
+
+    private void init(){
+        listview = findViewById(R.id.hashtag);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        listview.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemList = new ArrayList<>();
+        itemList.add("0");
+        itemList.add("1");
+        itemList.add("2");
+        itemList.add("3");
+        itemList.add("4");
+        itemList.add("5");
+        itemList.add("6");
+        itemList.add("7");
+        itemList.add("8");
+        itemList.add("9");
+        itemList.add("10");
+        itemList.add("testing~~~");
+
+        adapter = new MyAdapter(this, itemList, onClickItem);
+        listview.setAdapter(adapter);
+
+        MyListDecoration decoration = new MyListDecoration();
+        listview.addItemDecoration(decoration);
+    }
+
+    private View.OnClickListener onClickItem = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String str = (String) v.getTag();
+            Toast.makeText(QnAActivity.this, str, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     private void setupBottomNavigationView(){
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
