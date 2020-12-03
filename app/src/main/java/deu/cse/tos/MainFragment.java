2b0530f1d6ac9254d091a10183464c0abf4e506c
김영화 <kyha97@naver.com>
2020-12-04 07:35:44 +0900

    Add New Activity

--- app/src/main/java/deu/cse/tos/MainFragment.java
+++ app/src/main/java/deu/cse/tos/MainFragment.java
@@ -1,9 +1,12 @@
 package deu.cse.tos;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -80,9 +84,23 @@ public class MainFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageButton button = (ImageButton) getView().findViewById(R.id.brushimageButton);
+        Intent testIntent = new Intent(getActivity(), AddBrushListActivity.class);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //startActivity(i);
+                startActivity(testIntent);
+            }
+        });
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
