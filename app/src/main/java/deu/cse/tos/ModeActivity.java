1cde9c10c92b62ba77494405c5f7a0fb640f6965
SWKANG0525 <kanghodong@kakao.com>
2020-12-04 03:04:03 +0900

    Change Activities Layout to Fragment

--- app/src/main/java/deu/cse/tos/ModeActivity.java
+++ app/src/main/java/deu/cse/tos/ModeActivity.java
@@ -35,7 +35,6 @@ public class ModeActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
-        setupBottomNavigationView();
 
         ImageButton button = (ImageButton) findViewById(R.id.imageButtonmode1);
         Intent i = new Intent(this, VideoActivity.class);
@@ -51,13 +50,7 @@ public class ModeActivity extends AppCompatActivity {
 
     }
 
-    private void setupBottomNavigationView(){
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-        BottomNavigationHelper.enableNavigation(mContext, bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-    }
+
 
 
 }
