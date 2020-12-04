2a01fd7701b56d7fba716944647ccc254340219f
Hwang Jeong Ho <jakchung1021@gmail.com>
2020-12-04 15:38:46 +0900

    Create Information Act & CheckResult, Add Anim

--- app/src/main/java/deu/cse/tos/InformationActivity.java
+++ app/src/main/java/deu/cse/tos/InformationActivity.java
@@ -9,7 +9,13 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -17,11 +23,16 @@ import java.util.Calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.models.BarModel;
+
 public class InformationActivity extends AppCompatActivity {
     private Context mContext = InformationActivity.this;
     private static final int ACTIVITY_NUM = 4;
     private static final String TAG = "InformationActivity";
-
+    Spinner spinner;
+    BarChart chart2;
+    ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Window window = getWindow();
@@ -39,6 +50,49 @@ public class InformationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_information);
         setupBottomNavigationView();
 
+        //chartbar
+        BarChart mBarChart = (BarChart) findViewById(R.id.barchart);
+
+        mBarChart.addBar(new BarModel("JAN",70f, 0xFF98BFBD));
+        mBarChart.addBar(new BarModel("FEB",40f,  0xFF98BFBD));
+        mBarChart.addBar(new BarModel("MAR",45f, 0xFF98BFBD));
+        mBarChart.addBar(new BarModel("APR",55f, 0xFF98BFBD));
+        mBarChart.addBar(new BarModel("MAY",60f, 0xFF98BFBD));
+        mBarChart.addBar(new BarModel("JUNE",35,  0xFF98BFBD));
+        mBarChart.addBar(new BarModel("JULY",50f, 0xFF98BFBD));
+        mBarChart.addBar(new BarModel("AUG",60f,  0xFF98BFBD));
+
+        mBarChart.bringToFront();
+        mBarChart.startAnimation();
+
+        spinner = findViewById(R.id.spinner);
+        ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.nal, android.R.layout.simple_spinner_dropdown_item);
+        //R.array.test는 저희가 정의해놓은 1월~12월 / android.R.layout.simple_spinner_dropdown_item은 기본으로 제공해주는 형식입니다.
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(monthAdapter); //어댑터에 연결해줍니다.
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            } //이 오버라이드 메소드에서 position은 몇번째 값이 클릭됬는지 알 수 있습니다.
+            //getItemAtPosition(position)를 통해서 해당 값을 받아올수있습니다.
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
+        spinner.bringToFront();
+
+        Animation bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //Hooks
+        image = findViewById(R.id.image_view);
+
+
+
+        image.setAnimation(bottomAnim);
+
+
     }
 
     private void setupBottomNavigationView(){
@@ -48,6 +102,10 @@ public class InformationActivity extends AppCompatActivity {
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
     }
+
+
+
+
 
 
 }
