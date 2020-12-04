21d22d44677e773d992a1d995c36e65637507f61
Hwang Jeong Ho <jakchung1021@gmail.com>
2020-12-04 15:54:53 +0900

    Fix Some Bugs

--- app/src/main/java/deu/cse/tos/CheckResultActivity.java
+++ app/src/main/java/deu/cse/tos/CheckResultActivity.java
@@ -43,7 +43,6 @@ public class CheckResultActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkresult);
-        setupBottomNavigationView();
 
         BarChart mBarChart2 = (BarChart) findViewById(R.id.barchart2);
 
@@ -59,13 +58,6 @@ public class CheckResultActivity extends AppCompatActivity {
     }
 
 
-    private void setupBottomNavigationView(){
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-        BottomNavigationHelper.enableNavigation(mContext, bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setChecked(true);
-    }
 
 
 
