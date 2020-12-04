9f41ba1eec61f434d6ae111bb30ea398cd0d5e07
Chaebin Park <coqls0219@naver.com>
2020-12-05 06:34:41 +0900

    fcking shit

--- app/src/main/java/deu/cse/tos/ShowQnAActivity.java
+++ app/src/main/java/deu/cse/tos/ShowQnAActivity.java
@@ -3,13 +3,25 @@ package deu.cse.tos;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ShowQnAActivity extends AppCompatActivity {
     private Context mContext = this;
@@ -17,6 +29,13 @@ public class ShowQnAActivity extends AppCompatActivity {
     private static final String TAG = "ShowQnAActivity";
     private RecyclerView recyclerView;
     private MyHashAdapter hashAdapter;
+    private Button btn_arrow;
+    private Toolbar toolbar;
+    private ActionBar actionBar;
+    private String question;
+    private String answer;
+    private TextView tv_question;
+    private TextView tv_answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +52,46 @@ public class ShowQnAActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_qn_a);
+        this.question = (String) getIntent().getSerializableExtra("question");
+        this.answer = (String) getIntent().getSerializableExtra("answer");
+
+        this.tv_question = (TextView)findViewById(R.id.tv_question_intent);
+        this.tv_answer = (TextView)findViewById(R.id.tv_answer_intent);
+
+        this.tv_question.setText(this.question);
+        this.tv_answer.setText(this.answer);
+
+        initActionBar();
     }
 
+    public void initActionBar(){
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);//기본 제목을 없애줍니다.
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
 
-    private View.OnClickListener onClickItem = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String str = (String) v.getTag();
-            Toast.makeText(ShowQnAActivity.this, str, Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_customactionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout:
+                break;
+            case R.id.account:
+                break;
+            case android.R.id.home:
+                finish();
+                break;
         }
-    };
+        return super.onOptionsItemSelected(item);
+    }
 }
