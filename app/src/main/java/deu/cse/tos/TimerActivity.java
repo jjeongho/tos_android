f64587dd8baad5cbb95562c678e8bd995bf325da
Jun Kyu Bin <jgj9883@naver.com>
2020-12-05 18:56:16 +0900

    video and timer revise

--- app/src/main/java/deu/cse/tos/TimerActivity.java
+++ app/src/main/java/deu/cse/tos/TimerActivity.java
@@ -2,6 +2,7 @@ package deu.cse.tos;
 
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -10,6 +11,8 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -53,6 +56,17 @@ public class TimerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(0x00000000);  // transparent
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            int flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            window.addFlags(flags);
+        }
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
 
         super.onCreate(savedInstanceState);
@@ -132,14 +146,12 @@ public class TimerActivity extends AppCompatActivity {
                 }else if (time.equals("02:30")) {
                     textView.setText("앞니 안쪽을 20초 동안 닦으세요 !!") ;
                     handler.sendEmptyMessage(0);
-<<<<<<< HEAD
-=======
+
                     animationView.pauseAnimation();
                     animationView.setVisibility(View.INVISIBLE);
                     animationView = findViewById(R.id.tooth_top);
                     animationView.setVisibility(View.VISIBLE);
                     animationView.playAnimation();
->>>>>>> refs/remotes/origin/main
                 }else if (time.equals("02:50")) {
                     textView.setText("윗니 안쪽을 20초 동안 닦으세요 !!") ;
                     handler.sendEmptyMessage(0);
