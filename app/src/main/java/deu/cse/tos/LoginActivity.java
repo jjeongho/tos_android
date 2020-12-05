c726103e08352e10f74ac53cca878eb0f50360d9
Hwang Jeong Ho <jakchung1021@gmail.com>
2020-12-05 12:57:19 +0900

    Information & checkresult button edit

--- app/src/main/java/deu/cse/tos/LoginActivity.java
+++ app/src/main/java/deu/cse/tos/LoginActivity.java
@@ -35,7 +35,7 @@ import kotlin.jvm.functions.Function2;
 public class LoginActivity extends AppCompatActivity {
 
     Animation topAnim, bottomAnim;
-    ImageView image, image2;
+    ImageView image,image2;
 
 
     @Override
@@ -54,179 +54,134 @@ public class LoginActivity extends AppCompatActivity {
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mode);
-        ImageButton button = (ImageButton) findViewById(R.id.imageButton2);
-        Intent i = new Intent(this, ModeActivity.class);
+        ImageButton button = (ImageButton)findViewById(R.id.imageButton2);
+        Intent i = new Intent(this,ModeActivity.class);
         startActivity(i);
-        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+//        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+//        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
-        //Hooks
-        image = findViewById(R.id.imageView2);
-        image2 = findViewById(R.id.imageView);
-
-
-        //image.setAnimation(bottomAnim);
-        image2.setAnimation(topAnim);
-
-        final LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.tooth_turn);
-        animationView.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                animationView.setVisibility(View.GONE);
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-
-
-        getHashKey();
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if (LoginClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
-                    LoginClient.getInstance().loginWithKakaoTalk(LoginActivity.this, new Function2<OAuthToken, Throwable, Unit>() {
-                        @Override
-                        public Unit invoke(OAuthToken token, Throwable error) {
-                            if (error != null) {
-                                Log.e("TAG", "로그인 실패", error);
-                            } else {
-                                Log.d("TAG", "로그인 성공");
-
-                                // 사용자 정보 요청
-                                UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-                                    @Override
-                                    public Unit invoke(User user, Throwable error) {
-                                        if (error != error) {
-                                            Log.e("TAG", "사용자 정보 요청 실패", error);
-                                        } else {
-                                            UserAccount.getInstance().setHash_key(String.valueOf(user.getId()));
-
-                                            HashMap<String, Object> input = new HashMap<>();
-                                            input.put("hash_key", UserAccount.getInstance().getHash_key());
-                                            retrofitCheckAccountInterface.postCheckUserResult(input).enqueue(new Callback<CheckResult>() {
-
-                                                @Override
-                                                public void onResponse(Call<CheckResult> call, Response<CheckResult> response) {
-                                                    if (response.isSuccessful()) {
-                                                        CheckResult checkResult = response.body();
-                                                        if (checkResult.getResult().equals("false")) {
-                                                            HashMap<String, Object> input = new HashMap<>();
-                                                            input.put("hash_key", UserAccount.getInstance().getHash_key().toString());
-                                                            input.put("nickname", user.getKakaoAccount().getProfile().getNickname().toString());
-                                                            if (user.getKakaoAccount().getGender().toString() == "FEMALE")
-                                                                input.put("gender", 0);
-                                                            else
-                                                                input.put("gender", 1);
-
-                                                            retrofitCheckAccountInterface.postInsertUserResult(input).enqueue(new Callback<CheckResult>() {
-
-                                                                @Override
-                                                                public void onResponse(Call<CheckResult> call, Response<CheckResult> response) {
-                                                                    Log.d("Signup", "회원가입 성공");
-                                                                    startActivity(i);
-
-                                                                }
-
-                                                                @Override
-                                                                public void onFailure(Call<CheckResult> call, Throwable t) {
-                                                                    Log.d("Singup", "회원가입 실패");
-                                                                    t.printStackTrace();
-
-                                                                }
-                                                            });
-
-                                                        } else {
-                                                            Log.d("Else", "else");
-                                                            startActivity(i);
-
-                                                        }
-
-                                                    }
-                                                }
-
-                                                @Override
-                                                public void onFailure(Call<CheckResult> call, Throwable t) {
-                                                    Log.d("issigninfail", t.getMessage());
-                                                    t.getStackTrace();
-                                                }
-                                            });
-
-                                        }
+    //Hooks
+    //image = findViewById(R.id.imageView2);
+//        image2 = findViewById(R.id.imageView);
+//
+//
+//        //image.setAnimation(bottomAnim);
+//        image2.setAnimation(topAnim);
+//
+//        final LottieAnimationView animationView = (LottieAnimationView) findViewById(R.id.tooth_turn);
+//        animationView.addAnimatorListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                animationView.setVisibility(View.GONE);
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
 
 
-                                        return null;
+//        getHashKey();
 
-                                    }
-                                });
-                            }
-                            return null;
-                        }
-                    });
-                } else {
-                    LoginClient.getInstance().loginWithKakaoAccount(LoginActivity.this, new Function2<OAuthToken, Throwable, Unit>() {
-                        @Override
-                        public Unit invoke(OAuthToken token, Throwable error) {
-                            if (error != null) {
-                                Log.e("TAG", "로그인 실패", error);
-                            } else {
-                                Log.d("TAG", "로그인 성공");
-                                startActivity(i);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view)
+//            {
+//                startActivity(i);
 
-                                // 사용자 정보 요청
-                                UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-                                    @Override
-                                    public Unit invoke(User user, Throwable error) {
-                                        if (error != error) {
-                                            Log.e("TAG", "사용자 정보 요청 실패", error);
-                                        } else {
-                                            Log.i("TAG", user.toString());
-                                        }
-                                        return null;
-                                    }
-                                });
-                            }
-                            return null;
-                        }
-                    });
-                }
+//                if (LoginClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
+//                    LoginClient.getInstance().loginWithKakaoTalk(LoginActivity.this, new Function2<OAuthToken, Throwable, Unit>() {
+//                        @Override
+//                        public Unit invoke(OAuthToken token, Throwable error) {
+//                            if (error != null) {
+//                                Log.e("TAG", "로그인 실패", error);
+//                            } else {
+//                                Log.d("TAG", "로그인 성공");
+//                                startActivity(i);
+//
+//                                // 사용자 정보 요청
+//                                UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+//                                    @Override
+//                                    public Unit invoke(User user, Throwable error) {
+//                                        if (error != error) {
+//                                            Log.e("TAG", "사용자 정보 요청 실패", error);
+//                                        } else {
+//                                            Log.i("TAG", user.toString());
+//                                        }
+//                                        return null;
+//                                    }
+//                                });
+//                            }
+//                            return null;
+//                        }
+//                    });
+//                } else {
+//                    LoginClient.getInstance().loginWithKakaoAccount(LoginActivity.this,  new Function2<OAuthToken, Throwable, Unit>() {
+//                        @Override
+//                        public Unit invoke(OAuthToken token, Throwable error) {
+//                            if (error != null) {
+//                                Log.e("TAG", "로그인 실패", error);
+//                            } else {
+//                                Log.d("TAG", "로그인 성공");
+//                                startActivity(i);
+//
+//                                // 사용자 정보 요청
+//                                UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+//                                    @Override
+//                                    public Unit invoke(User user, Throwable error) {
+//                                        if (error != error) {
+//                                            Log.e("TAG", "사용자 정보 요청 실패", error);
+//                                        } else {
+//                                            Log.i("TAG", user.toString());
+//                                        }
+//                                        return null;
+//                                    }
+//                                });
+//                            }
+//                            return null;
+//                        }
+//                    });
+//                }
+//
+}
+//        });
+//
+//    }
+//
+//    private void getHashKey(){
+//        PackageInfo packageInfo = null;
+//        try {
+//            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        if (packageInfo == null)
+//            Log.e("KeyHash", "KeyHash:null");
+//
+//        for (Signature signature : packageInfo.signatures) {
+//            try {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            } catch (NoSuchAlgorithmException e) {
+//                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+//            }
+//        }
+//    }
+//
 
-            }
-        });
-
-    }
-
-    private void getHashKey() {
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
 
 
-            }
+
+}
