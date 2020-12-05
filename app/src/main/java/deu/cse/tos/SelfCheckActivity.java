510a47649dcb2cf06eb97bd9f3efdb289317e467
Jun Kyu Bin <jgj9883@naver.com>
2020-12-05 13:59:12 +0900

    activate

--- app/src/main/java/deu/cse/tos/SelfCheckActivity.java
+++ app/src/main/java/deu/cse/tos/SelfCheckActivity.java
@@ -1,32 +1,26 @@
 package deu.cse.tos;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.kakao.sdk.auth.LoginClient;
-import com.kakao.sdk.auth.model.OAuthToken;
-import com.kakao.sdk.user.UserApiClient;
-import com.kakao.sdk.user.model.User;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-import kotlin.Unit;
-import kotlin.jvm.functions.Function2;
 
-public class SelfCheckActivity extends AppCompatActivity {
 
+public class SelfCheckActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView selftext1;
+    TextView selftext2;
+    TextView selftext3;
+    TextView selftext4;
+    CheckBox check1 = (CheckBox) findViewById(R.id.checkbox1) ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +38,52 @@ public class SelfCheckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selfcheck);
 
-        Intent i = new Intent(this, CheckResultActivity.class);
+        selftext1 = (TextView) findViewById(R.id.selfcheck1);
+        selftext1.setOnClickListener(this);
+        selftext2 = (TextView) findViewById(R.id.selfcheck2);
+        selftext2.setOnClickListener(this);
+        selftext3 = (TextView) findViewById(R.id.selfcheck3);
+        selftext3.setOnClickListener(this);
+        selftext4 = (TextView) findViewById(R.id.selfcheck4);
+        selftext4.setOnClickListener(this);
+
+
+        //Intent i = new Intent(this, CheckResultActivity.class);
 
 
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.selfcheck1:
+                // TextView가 클릭될 시 할 코드작성
+                if(check1.isChecked()){
+                    check1.setChecked(false);
+                }else {
+                    check1.setChecked(true);
+                }
+
+                System.out.println("1");
+                break;
+
+            case R.id.selfcheck2:
+                // TextView가 클릭될 시 할 코드작성
+
+                System.out.println("2");
+                break;
+            case R.id.selfcheck3:
+                // TextView가 클릭될 시 할 코드작성
+
+                System.out.println("3");
+                break;
+            case R.id.selfcheck4:
+                // TextView가 클릭될 시 할 코드작성
+
+                System.out.println("4");
+                break;
+        }
+    }
 }
